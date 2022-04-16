251e66fca38e4113533bbc2c6238986931f40aa6
Benjamin Mirisola <bennymirisola@gmail.com>
2022-04-16 17:39:18 -0400

    Initial commit

---
+++ showtext.h
@@ -0,0 +1,15 @@
+//
+// Created by bmirisola on 4/16/22.
+//
+
+#include <gtk/gtk.h>
+
+
+#ifndef CPE553_FINAL_PROJECT_SHOWTEXT_H
+#define CPE553_FINAL_PROJECT_SHOWTEXT_H
+
+static void destroy(GtkWidget *widget, gpointer data){
+    gtk_main_quit ();
+}
+
+#endif //CPE553_FINAL_PROJECT_SHOWTEXT_H
