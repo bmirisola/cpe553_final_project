1427635b3ae7f7766ca363577459f59851d7e4c6
Benjamin Mirisola <bennymirisola@gmail.com>
2022-04-17 01:21:40 -0400

    implemented window class

--- gtk/Window.cu
+++ gtk/Window.cu
@@ -3,3 +3,28 @@
 //
 
 #include "Window.cuh"
+
+Window::Window() : windowName("Default") {
+    gtk_window_set_title(GTK_WINDOW(window), "Default");
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+    g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_container_add (GTK_CONTAINER (window), grid);
+}
+
+Window::Window(const gchar *windowName, int width, int height): windowName(windowName) {
+    gtk_window_set_title(GTK_WINDOW(window), windowName);
+    gtk_window_set_default_size(GTK_WINDOW(window), width, height);
+    g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_container_add (GTK_CONTAINER (window), grid);
+}
+
+Window::~Window() {
+    delete window;
+    delete grid;
+}
+
+GtkWidget *Window::getGrid() {
+    return grid;
+}
--- gtk/Window.cuh
+++ gtk/Window.cuh
@@ -5,8 +5,17 @@
 #ifndef CPE553_FINAL_PROJECT_WINDOW_CUH
 #define CPE553_FINAL_PROJECT_WINDOW_CUH
 
+#include "gtk/gtk.h"
 
 class Window {
+    GtkWidget *window, *grid;
+    const gchar *windowName;
+
+    Window();
+    Window(const gchar *windowName, int width, int height);
+    ~Window();
+    GtkWidget *getGrid();
+
 
 };
 
