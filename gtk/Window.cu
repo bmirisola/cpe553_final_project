1da7cf7b14f345f37487f1247a8c7be8773adc57
Benjamin Mirisola <bennymirisola@gmail.com>
2022-04-17 00:34:12 -0400

    added window class

---
+++ gtk/Window.cu
@@ -0,0 +1,5 @@
+//
+// Created by bmirisola on 4/17/22.
+//
+
+#include "Window.cuh"
---
+++ gtk/Window.cuh
@@ -0,0 +1,14 @@
+//
+// Created by bmirisola on 4/17/22.
+//
+
+#ifndef CPE553_FINAL_PROJECT_WINDOW_CUH
+#define CPE553_FINAL_PROJECT_WINDOW_CUH
+
+
+class Window {
+
+};
+
+
+#endif //CPE553_FINAL_PROJECT_WINDOW_CUH
